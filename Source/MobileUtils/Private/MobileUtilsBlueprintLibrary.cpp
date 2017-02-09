f8aec69d2569e4a16d81fdbd22f60e7c1a8ec0ea
gameDNAstudio <office@gamednastudio.com>
2017-02-09 19:18:52 +0100

    Update credits

--- Source/MobileUtils/Private/MobileUtilsBlueprintLibrary.cpp
+++ Source/MobileUtils/Private/MobileUtilsBlueprintLibrary.cpp
@@ -1,6 +1,6 @@
 // Mobile Utils Plugin
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2017 gameDNA studio. All Rights Reserved.
+// Copyright (c) 2014-2017 gameDNA. All Rights Reserved.
 
 #include "MobileUtilsPrivatePCH.h"
 #include "MobileUtilsBlueprintLibrary.h"
