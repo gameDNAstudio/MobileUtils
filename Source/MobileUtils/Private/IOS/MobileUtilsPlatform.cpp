5986e847f1b0d9f84e92f794986415bb36e850a1
gameDNA <office@gamednastudio.com>
2019-02-20 00:18:50 +0000

    Update copyrights

--- Source/MobileUtils/Private/IOS/MobileUtilsPlatform.cpp
+++ Source/MobileUtils/Private/IOS/MobileUtilsPlatform.cpp
@@ -1,6 +1,6 @@
 // Mobile Utils Plugin
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2018 gameDNA Ltd. All Rights Reserved.
+// Copyright (c) 2014-2019 gameDNA Ltd. All Rights Reserved.
 
 #include "MobileUtilsPlatform.h"
 
