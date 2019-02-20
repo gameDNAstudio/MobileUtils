438ba73737fbf4ee3876dd6c43431a77f7d46d6b
gameDNA <office@gamednastudio.com>
2019-02-20 00:04:51 +0000

    Update to UE 4.22

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 
 We want to make a big mobile plugin with many features from community so feel free to make pull request! After review, if all is good, it will be merged into plugin. Thank you! :-)
 
-* **Current version:** 1.10.0
-* **Binaries compiled for:** Unreal Engine 4.21
-* **Required Unreal Engine 4.21 or above.**
+* **Current version:** 1.11.0
+* **Binaries compiled for:** Unreal Engine 4.22
+* **Required Unreal Engine 4.22 or above.**
 
 ## Features
 * Checking internet connection availability
--- Source/MobileUtils/MobileUtils.Build.cs
+++ Source/MobileUtils/MobileUtils.Build.cs
@@ -1,6 +1,6 @@
 // Mobile Utils Plugin
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2018 gameDNA Ltd. All Rights Reserved.
+// Copyright (c) 2014-2019 gameDNA Ltd. All Rights Reserved.
 
 using System.IO;
 
@@ -33,14 +33,14 @@ namespace UnrealBuildTool.Rules
 			if (Target.Platform == UnrealTargetPlatform.IOS)
 			{
 				PublicAdditionalFrameworks.Add(
-					new UEBuildFramework(
+					new Framework(
 						"Reachability",
 						"../ThirdParty/IOS/Reachability.embeddedframework.zip"
 					)
 				);
 
 				PublicAdditionalFrameworks.Add(
-					new UEBuildFramework(
+					new Framework(
 						"SSKeychain",
 						"../ThirdParty/IOS/SSKeychain.embeddedframework.zip"
 					)
